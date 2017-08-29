c4beee878429a0490b80b057ec08c4466fef3c1c
Jesse Shawl <jesse@jshawl.com>
2017-08-29 18:21:38 -0400

    initial commit

--- src/stopwatch/core.clj
+++ src/stopwatch/core.clj
@@ -1,9 +1,16 @@
 (ns stopwatch.core
   (:gen-class))
 
+(defn set-interval [callback ms] 
+  (future (while true (do (Thread/sleep ms) (flush) (callback)))))
+
+(defn now []
+  (quot (System/currentTimeMillis) 1000))
+
+(defn job [start] (set-interval #(print 
+  (str "\r" (- (now) start) ))1000))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (doseq [i (range 10000000)]
-    (print (str "\r" i))))
+    (job (now)))
