a218bc658b072d382ee5690165f1539c511bef74
Jesse Shawl <jesse@jshawl.com>
2017-08-30 09:39:32 -0400

    add duration format

--- src/stopwatch/core.clj
+++ src/stopwatch/core.clj
@@ -1,4 +1,5 @@
 (ns stopwatch.core
+  (:require [clojure.string :refer [trim]])
   (:gen-class))
 
 (defn set-interval [callback ms] 
@@ -7,8 +8,22 @@
 (defn now []
   (quot (System/currentTimeMillis) 1000))
 
+(defn duration [s]
+  (let [hours (int (Math/floor (/ s 3600))) l (- s (* hours 3600))]
+    (let [minutes (int (Math/floor (/ (- s (* hours 3600)) 60)))]
+      (let [seconds (- s (+ (* minutes 60) (* hours 3600)))]
+        {:hour hours :minute minutes :second seconds}))))
+
+(defn fmt [k v]
+  (if (zero? v) "" (str v " " (name k) (if (> v 1) "s" ""))))
+
+(defn duration-fmt [s]
+  (reduce-kv 
+    (fn [m k v]
+      (trim (str m " " (fmt k v)))) "" (duration s)))
+
 (defn job [start] (set-interval #(print 
-  (str "\r" (- (now) start) ))1000))
+  (str "\r" (duration-fmt (- (now) start)))) 1000))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- test/stopwatch/core_test.clj
+++ test/stopwatch/core_test.clj
@@ -2,6 +2,19 @@
   (:require [clojure.test :refer :all]
             [stopwatch.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest duration-test
+  (testing "duration"
+    (is (= (duration 1) {:hour 0 :minute 0 :second 1}))
+    (is (= (duration 2) {:hour 0 :minute 0 :second 2}))
+    (is (= (duration 60) {:hour 0 :minute 1 :second 0}))
+    (is (= (duration 61) {:hour 0 :minute 1 :second 1}))
+    (is (= (duration 3666) {:hour 1 :minute 1 :second 6}))
+    ))
+
+(deftest duration-fmt-test
+(testing "duration formatted"
+  (is (= (duration-fmt 1) "1 second"))
+  (is (= (duration-fmt 2) "2 seconds"))
+  (is (= (duration-fmt 60) "1 minute"))
+  (is (= (duration-fmt 62) "1 minute 2 seconds"))
+  ))
